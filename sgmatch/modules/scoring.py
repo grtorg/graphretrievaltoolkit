1a74e5ccf711818f22837e4f5bda0fb687133d13
kunind27 <kunind444@gmail.com>
2023-02-11 16:58:43 +0530

    Added Namespace args & Graph Matcher base class

--- sgmatch/modules/scoring.py
+++ sgmatch/modules/scoring.py
@@ -76,3 +76,5 @@ def similarity(h_i, h_j, mode:str = "cosine"):
     if mode == "hinge":
         prod = 1 - torch.matmmul(h_i, h_j)
         return torch.max(torch.zeros(prod.shape), prod)
+    if mode == "min_sum":
+        return torch.sum(torch.min(h_i, h_j), dim=-1)
