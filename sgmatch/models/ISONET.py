faea75f0a34fa11e248143129be593d99c99b747
kunind27 <kunind444@gmail.com>
2023-02-17 02:27:50 +0530

    Added ISONET model

--- sgmatch/models/ISONET.py
+++ sgmatch/models/ISONET.py
@@ -1,11 +1,134 @@
 from typing import List, int, Optional, Dict
 import torch
 from torch import nn
+from torch.functional import Tensor
+from torch_geometric.utils import to_dense_adj
+
+from ..modules.encoder import MLPEncoder
+from ..modules.scoring import GumbelSinkhornNetwork
+from ..modules.propagation import GraphProp
+from ..utils.utility import setup_linear_nn, setup_LRL_nn
 
 class ISONET(nn.Module):
-    def __init__(self, args):
+    r"""End-to-End implementation of the ISONET model from the - `"Interpretable Neural Subgraph Matching for Graph Retrieval" 
+    <https://ojs.aaai.org/index.php/AAAI/article/view/20784>`_ paper.
+    TODO: Add argument description
+    """
+    def __init__(self, node_feature_dim: int, enc_node_hidden_sizes: List[int], 
+                prop_node_hidden_sizes: List[int], prop_message_hidden_sizes: List[int],
+                edge_feature_dim: Optional[int] = None, enc_edge_hidden_sizes: Optional[List[int]] = None,
+                message_net_init_scale: float = 0.1, node_update_type: str = 'GRU', 
+                use_reverse_direction: bool = True, reverse_dir_param_different: bool = True, layer_norm: bool = False, 
+                lrl_hidden_sizes: List[int] = [16, 16], temp: float = 0.1, eps: float = 1e-20, 
+                noise_factor: float = 1, gs_num_iters: int = 20):
         super(ISONET, self).__init__()
-        self.args = args
+        self.node_feature_dim = node_feature_dim
+        self.edge_feature_dim = edge_feature_dim
 
-    def forward(self):
-        pass
+        # Encoder Module        
+        self.enc_node_layers = enc_node_hidden_sizes
+        self.enc_edge_layers = enc_edge_hidden_sizes
+        
+        # Propagation Module
+        self.prop_node_layers = prop_node_hidden_sizes
+        self.prop_message_layers = prop_message_hidden_sizes
+
+        self.message_net_init_scale = message_net_init_scale # Unused
+        self.node_update_type = node_update_type
+        self.use_reverse_direction = use_reverse_direction
+        self.reverse_dir_param_different = reverse_dir_param_different
+
+        self.layer_norm = layer_norm
+        self.prop_type = "embedding"
+
+        # Gumbel-Sinkhorn Network Parameters
+        self.lrl_hidden_sizes = lrl_hidden_sizes
+        self.temp = temp
+        self.eps = eps
+        self.noise_factor = noise_factor
+        self.gs_num_iters = gs_num_iters
+
+    def setup_layers(self):
+        # Used the same nomenclature present in the ISONET paper
+        self._init = MLPEncoder(self.node_feature_dim, self.enc_node_layers, edge_feature_dim=self.edge_feature_dim, 
+                                   edge_hidden_sizes=self.enc_edge_layers)
+        
+        self._message_agg_comb = GraphProp(self.enc_node_layers[-1], self.prop_node_layers, self.prop_message_layers, 
+                               edge_feature_dim=self.edge_feature_dim, message_net_init_scale=self.message_net_init_scale,
+                               node_update_type=self.node_update_type, use_reverse_direction=self.use_reverse_direction,
+                               reverse_dir_param_different=self.reverse_dir_param_different, layer_norm=self.layer_norm,
+                               prop_type=self.prop_type)
+        
+        self.LRL = setup_LRL_nn(input_dim=self.prop_message_layers[-1], hidden_sizes=self.lrl_hidden_sizes)
+
+        self.gumbel_sinkhorn = GumbelSinkhornNetwork(self.temp, self.eps, self.noise_factor, self.gs_num_iters)
+        
+    def reset_parameters(self):
+        self._init.reset_parameters()
+        self._message_agg_comb.reset_parameters()
+        for layer in self.LRL[::2]:
+            layer.reset_parameters()
+
+    def embed_edges(self, node_features: Tensor, edge_index: Tensor, edge_features: Optional[Tensor] = None, num_prop: int = 5):
+        from_idx = edge_index[:,0] if len(edge_index.shape) == 3 else edge_index[0]
+        to_idx = edge_index[:,1] if len(edge_index.shape) == 3 else edge_index[1]
+
+        if edge_features is not None:
+            node_features, edge_features = self._init(node_features, edge_features)
+        else:
+            node_features = self._init(node_features)
+        
+        # This calculates h_u(K)
+        for _ in range(num_prop):
+            # TODO: Can include a list keeping track of propagation layer outputs
+            node_features = self._message_agg_comb(node_features, from_idx, to_idx, edge_features)
+
+        # Computes r_(u,v)_(K)
+        edge_message = self._message_agg_comb._compute_messages(node_features, from_idx, to_idx,
+                                                                        self._message_agg_comb.message_net,
+                                                                        edge_features=edge_features)
+        #reverse_edge_message = self._message_agg_comb._compute_messages(node_features, to_idx, from_idx,
+                                                                        #self._message_agg_comb.message_net,
+                                                                        #edge_features=edge_features)
+        return edge_message
+
+
+    def forward(self, node_features_q: Tensor, node_features_c: Tensor, edge_index_q: Tensor, edge_index_c: Tensor,
+                edge_features_q: Optional[Tensor] = None, edge_features_c: Optional[Tensor] = None, 
+                batch_q: Optional[Tensor] = None, batch_c: Optional[Tensor] = None, num_prop: int = 5):
+        
+        # Computes r_(u,v)_(K)
+        edge_features_q = self.embed_edges(node_features_q, edge_index_q, edge_features_q, num_prop)
+        edge_features_c = self.embed_edges(node_features_c, edge_index_c, edge_features_c, num_prop)
+
+
+        # Once we have the Node and Edge Embeddings, we create R_q and R_c Matrices
+        if len(edge_index_q.shape)==3 and len(edge_index_c.shape)==3:
+            # Finding out the maximum num of edges in any graph - query / corpus in the batch
+            max_num_edges = max([edge_index.shape[1].item() for edge_index in edge_index_q])
+            max_num_edges = max(max_num_edges, max([edge_index.shape[1].item() for edge_index in edge_index_c]))
+
+            edge_features_q_batched = torch.stack([torch.functional.pad(x, pad=(0,0,0,max_num_edges-x.shape[0]))\
+                                                   for x in edge_features_q])
+            edge_features_c_batched = torch.stack([torch.functional.pad(x, pad=(0,0,0,max_num_edges-x.shape[0]))\
+                                                   for x in edge_features_c])
+        else:
+            edge_features_q_batched = to_dense_adj(edge_index=edge_index_q, batch=batch_q, edge_attr=edge_features_q)
+            edge_features_c_batched = to_dense_adj(edge_index=edge_index_c, batch=batch_c, edge_attr=edge_features_c)
+
+        
+        # Passing R_q and R_c through the LRL and the Gumbel-Sinkhorn Network
+        edge_features_q_batched_lrl = self.LRL(edge_features_q_batched)
+        edge_features_c_batched_lrl = self.LRL(edge_features_c_batched)
+        soft_permutation_matrix = self.gumbel_sinkhorn(torch.matmul(edge_features_q_batched_lrl, 
+                                                                    edge_features_c_batched_lrl.permute(0,2,1)))
+
+        # Calculating the Distance between corpus and query graph using the Soft Permutation Matrix
+        d_cq = nn.ReLU(edge_features_q_batched - torch.matmul(soft_permutation_matrix, edge_features_c_batched))
+        d_cq = torch.sum(d_cq, dim=(1,2))
+        return d_cq
+
+
+    def __repr__(self) -> str:
+        # TODO: Update __repr__ with information
+        return super().__repr__()
